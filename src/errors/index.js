8fef93cc889a85a99c025a98204cc56a679b5e18
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-04-04 01:13:55 -0500

    Added folder for error messages

---
+++ src/errors/index.js
@@ -0,0 +1,5 @@
+export const NO_INVENTORY_FOUND = 'No inventory Data found!\nEnsure file is a valid Player Log with "Detailed Logs'
++ 'enabled.\nDetailed Logs can be found in MTG Arena under:\n    Options -> Account -> Detailed Logs';
+
+export const INVALID_FILE = 'File not recognized!\nWindows users will typically find their ".log" files at:\n'
++'    %USERPROFILE%\\AppData\\LocalLow\\Wizards Of The Coast\\MTGA';
