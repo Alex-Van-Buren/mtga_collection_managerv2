d66439b3eeab18ede890252ca18a80b4be624200
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-04-05 13:04:02 -0500

    Resizing window now changes the number of cards displayed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import SetCard from './SetCard';
@@ -28,13 +28,52 @@ function renderSets(sets = ['eld', 'thb', 'iko', 'm21', 'znr', 'khm']) {
 }
 
 /**
+ * Helper function to stop React from rerendering continuously upon window resize
+ */
+ function debounce(func, delay) {
+    let timer;
+    return (() => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            timer = null;
+            func.apply(this, arguments);
+        }, delay);
+    });
+}
+
+/**
  * Home Page Component - Displays current card sets and other general info/options
  */
 function Home() {
+
+    // Use local state to track window size
+    const [size, setSize] = useState({ width: window.innerWidth });
+
+    // useEffect refreshes component upon window resize
+    useEffect(() => {
+        const debouncedResize = debounce(() => {
+            setSize({ width: window.innerWidth });
+        }, 400)
+
+        window.addEventListener('resize', debouncedResize);
+
+        // Clean up
+        return (() => { window.removeEventListener('resize', debouncedResize) });
+    });
+
+    // Choose how many cards to show based on window size
+    let numCards = "four"; // Default to full width
+    if (size.width < 500)
+        numCards = "one"
+    else if (size.width < 850)
+        numCards = "two";
+    else if (size.width < 1200)
+        numCards = "three";
+
     return(
         <div className="ui container">
-            <div className="ui huge center aligned header ">Standard Collection</div>
-            <div className="ui four stackable cards">
+            <div className="ui huge center aligned header">Standard Collection</div>
+            <div className={`ui ${numCards} cards`}>
                 {renderSets()}
             </div>
         </div>
