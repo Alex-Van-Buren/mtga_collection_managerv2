4e2867b24dfe6bbe4a2b193c87a1917b87558bd5
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-04-08 12:12:02 -0500

    Basic styling for SetDetailsOptions

--- src/components/SetDetails/ColorCheckboxes.js
+++ src/components/SetDetails/ColorCheckboxes.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { selectColor } from '../../actions';
 import CustomCheckbox from './CustomCheckbox';
+import '../../css/ColorCheckboxes.css';
 
 export default function ColorCheckboxes() {
 
@@ -15,15 +16,20 @@ export default function ColorCheckboxes() {
     // List card colors and create color checkboxes for each
     const colors = ['white', 'blue', 'black', 'red', 'green', 'multi', 'colorless'];
     const renderColors = colors.map(color => {
-        return <CustomCheckbox text={color} key={color} checked={colorValues[color]} 
-        onChange={(e) => dispatch( selectColor(e.target.name, e.target.checked) )}/>;
+        return (
+            <CustomCheckbox 
+                text={color} key={color} checked={colorValues[color]} 
+                labelClass="colorBoxInputLabel" inputClass="colorBoxInput"
+                onChange={(e) => dispatch( selectColor(e.target.name, e.target.checked) )}
+            />
+        );
     });
 
     return (
         
         // Checkboxes for color: White, Blue, Black, Red, Green, All Multicolored, Colorless
-        <div>
-            <label htmlFor="color">Color(s):</label>
+        <div className="colorBox">
+            <label htmlFor="color" className="colorBoxLabel">Color(s):</label>
             {renderColors}
         </div>
     );
