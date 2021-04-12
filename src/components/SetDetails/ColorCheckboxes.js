4bd8706fc6fcfd06438f3f11c9391d349d995eb7
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-04-12 13:49:28 -0500

    Update colors to have icons

--- src/components/SetDetails/ColorCheckboxes.js
+++ src/components/SetDetails/ColorCheckboxes.js
@@ -5,6 +5,15 @@ import { selectColor } from '../../actions';
 import CustomCheckbox from './CustomCheckbox';
 import '../../css/ColorCheckboxes.css';
 
+// Import SVG images
+import W from '../../images/W.svg';
+import U from '../../images/U.svg';
+import B from '../../images/B.svg';
+import R from '../../images/R.svg';
+import G from '../../images/G.svg';
+import M from '../../images/M.svg';
+import C from '../../images/C.svg';
+
 export default function ColorCheckboxes() {
 
     // Access the redux dispatcher
@@ -13,13 +22,24 @@ export default function ColorCheckboxes() {
     // Get currently selected colors from redux state
     const colorValues = useSelector( state => state.detailsOptions.colors );
 
-    // List card colors and create color checkboxes for each
+    // List card colors and the tags needed for their icons
     const colors = ['white', 'blue', 'black', 'red', 'green', 'multi', 'colorless'];
-    const renderColors = colors.map(color => {
+    const colorSVGs = [ W, U, B, R, G, M, C ];
+    
+    // Create color checkboxes for each
+    const renderColors = colors.map( (color, index) => {
         return (
-            <CustomCheckbox 
-                text={color} key={color} checked={colorValues[color]} 
+            <CustomCheckbox
+                // Data
+                text={color} key={color} checked={colorValues[color]}
+
+                // Icon
+                labelText={ <img className="colorIcon" src={colorSVGs[index]} alt="icon" /> }
+
+                // Classes
                 labelClass="colorBoxInputLabel" inputClass="colorBoxInput"
+
+                // Dispatch redux action
                 onChange={(e) => dispatch( selectColor(e.target.name, e.target.checked) )}
             />
         );
