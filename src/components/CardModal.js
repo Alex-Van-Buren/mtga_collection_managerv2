b6ba666fe96658f3cc9956b736a71fd59ba73d7f
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-04-19 13:09:46 -0500

    Modal working at full height

--- src/components/CardModal.js
+++ src/components/CardModal.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import Modal from './Modal';
 import { showModal, setModalContent } from '../actions';
+import '../css/CardModal.css';
 
 function CardModal() {
     const dispatch = useDispatch();
@@ -11,10 +12,38 @@ function CardModal() {
     const index = useSelector(state => state.modal.content);
     const imageList = useSelector(state => state.detailsOptions.imageList);
 
+    // Add listener for escape key
+    useEffect(() => {
+
+        // Function checks whether escape has been pressed
+        const onEscape = (event) => {
+            if (event.keyCode === 27)
+            dispatch(showModal(false));
+        }
+
+        // Add listener for function
+        window.addEventListener('keydown', onEscape);
+
+        // Cleanup function
+        return () => window.removeEventListener('keydown', onEscape);
+    }, [dispatch]);
+
     // Return nothing when modal not shown
     if (!show) {
         return null;
     }
+
+    const BUTTON_CLASS = "massive basic ui icon button";
+
+    // Close modal button
+    const exit = (
+        <button
+            onClick={() => dispatch(showModal(false))}
+            className={`exit ${BUTTON_CLASS}`}
+        >
+            <i className="big close icon"></i>
+        </button>
+    );
 
     // "Go to previous card" button to be rendered inside modal
     const prev = (
@@ -22,9 +51,9 @@ function CardModal() {
             // Update image to display if possible
             onClick={ () => { if (index > 0) dispatch(setModalContent(index - 1)) } }
             // Add "disabled" to class if not clickable (first image in list)
-            className={index <= 1 ? "disabled" : ""}
+            className={BUTTON_CLASS}
         >
-            <i class="chevron left icon"></i>
+            <i className="big chevron left icon"></i>
         </button>
     );
 
@@ -34,49 +63,50 @@ function CardModal() {
             // Update image to display if possible
             onClick={ () => { if (index < imageList.length-1) dispatch(setModalContent(index + 1)) } }
             // Add "disabled" to class if not clickable (first image in list)
-            className={index < imageList.length-1 ? "disabled" : ""}
+            className={BUTTON_CLASS}
         >
-            <i class="chevron right icon"></i>
+            <i className="big chevron right icon"></i>
         </button>
     );
-
-    const exit = (
-        <button onClick={() => closeModal()}>
-            <i className="close icon"></i>
-        </button>
-    );
-
-    // Handle closing the modal
-    function closeModal() {
-        dispatch(showModal(false));
-    }
-
-    // Render actinos
-    const renderedActions = <>
-
-        {/* Display previous button */}
-        {prev}
-
-        {/* Display next button */}
-        {next}
-
-        {/* Display exit button */}
-        {exit}
-    </>
 
     // Render Modal Content
-    const renderedContent = <>
+    const renderedContent =
+    <>
+        {/* Display exit button */}
+        <div className="modelExit">
+            {exit}
+        </div>
 
-        {/* Display current card image from imageList using index */}
-        <img src={imageList[index]} alt="Card"/>
-    </>        
+        <div className="modelContent">
+            {/* Display previous button */}
+            <div className="backButton">
+                {prev}
+            </div>
+
+            {/* Display current card image from imageList using index */}
+            <img src={imageList[index]} alt="Card" className="cardImage"/>
+
+            {/* Display next button */}
+            <div className="forwardButton">
+                {next}
+            </div>
+        </div>
+    </>
 
     // Render Modal
-    return <Modal
-        content={renderedContent}
-        actions={renderedActions}
-        onDismiss={() => closeModal()}
-    />
+    return createPortal(
+        <div
+            onClick={() => dispatch(showModal(false))}
+            className="ui dimmer modals visible active cardModal"
+        >
+            
+            <div onClick={e => e.stopPropagation()} >
+                {renderedContent}
+            </div>
+
+        </div>,
+        document.querySelector("#modal")
+    );
 }
 
 export default CardModal;
