411557b64728b72d003bc765de72f5ec645d6a00
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-04-19 17:38:00 -0500

    Reformatting and commenting. No functional differences.

--- src/components/SetDetails/CardList.js
+++ src/components/SetDetails/CardList.js
@@ -12,14 +12,16 @@ import '../../css/CardList.css';
  * @returns Returns a grid of images of card in the set using filter options that are retrieved from redux store. Also displays the number
  * of cards owned by the user above each card image. 
  */
-function CardList( {setId} ) {
+function CardList({ setId }) {
     
+    // Get values from redux state
     const cardCollection = useSelector(state => state.inventory.cardCollection);
+    const colors         = useSelector(state => state.detailsOptions.colors);
+    const searchTerm     = useSelector(state => state.detailsOptions.searchTerm);
+    const rarity         = useSelector(state => state.detailsOptions.rarity);
+    const showCards      = useSelector(state => state.detailsOptions.showCards);
 
-    const colors = useSelector(state => state.detailsOptions.colors);
-
-    const searchTerm = useSelector(state => state.detailsOptions.searchTerm);
-
+    // Access redux dispatch
     const dispatch = useDispatch();
 
     // Build array for color searchOption
@@ -44,15 +46,9 @@ function CardList( {setId} ) {
     if ( colorOption.length === 0) {
         colorOption = undefined;
     }
-
-    // Get rarity selected from redux
-    const rarity = useSelector(state => state.detailsOptions.rarity);
-
-    let rarityOption = [rarity];
-    // if rarity is set to all, change rarityOption to undefined so findCards will not filter by rarity
-    if ( rarity === 'all' ){
-        rarityOption = undefined;
-    }    
+    
+    // If rarity is set to all, set rarityOption to undefined so findCards will not filter by rarity
+    const rarityOption = rarity !== 'all' ? [rarity] : undefined;
     
     // Put all search options into a single object for findCards function
     const searchOptions = {set: setId, color: colorOption, booster: true, rarity: rarityOption, term: searchTerm};
@@ -63,100 +59,89 @@ function CardList( {setId} ) {
     // Get the cards using the findCards Function
     const cards = findCards(searchOptions, returnOptions);
 
-    // Get the showCards to know which cards to show
-    const showCards = useSelector(state => state.detailsOptions.showCards);
-
-    // Initialize number of cards shown with specified options
-    let numCardsShown = 0;
-
     // Track currently shown pictures
     let currentPictures = [];
 
     // render the cards based on how many the user owns and the showCards option
     const renderedCards = cards.map( (card) => {
 
+        // Get image from card
         const img = card.image_uris.border_crop;
-        let numOwned = 0;
 
-        // Make sure cardCollection is defined
-        if ( cardCollection && cardCollection[card.arenaId]) {
-            numOwned = cardCollection[card.arenaId];
-        } 
+        // Get number of card owned, if any
+        const numOwned = cardCollection && cardCollection[card.arenaId] ? 
+            cardCollection[card.arenaId] : 0;
+
         // Initialize makeCard Boolean to false
         let makeCard = false;
-        if ( showCards === 'all' ) {
+
+        // Execute logic from showCards variable to determine whether card should be made
+        if (( showCards === 'all' )                  ||
+            ( showCards === '=0' && numOwned === 0 ) ||
+            ( showCards === '>0' && numOwned > 0 )   ||
+            ( showCards === '<4' && numOwned < 4 )   ||
+            ( showCards === '=4' && numOwned === 4))
+        {
             makeCard = true;
         }
 
-        else if ( showCards === '=0' && numOwned === 0 ) {
-            makeCard = true;
+        // Don't make card if showCards logic says not to
+        if ( !makeCard ) {
+            return null;
         }
 
-        else if ( showCards === '>0' && numOwned >0 ) {
-            makeCard =  true;
-        }
+        // Track images to be displayed
+        currentPictures.push(img);
 
-        else if ( showCards === '<4' && numOwned <4 ) {
-            makeCard = true;
-        }
-
-        else if ( showCards === '=4' && numOwned === 4) {
-            makeCard = true;
-        }        
-
-        if ( makeCard ) {
-            numCardsShown++;
-            currentPictures.push(img);
-            return (
-                <div className="column" key={card.arenaId}>
-                    <div className="ui fluid card removeBoxShadow">
-                        <div className="content">
-                            <div className="right floated content" >{numOwned} / 4 </div>
-                        </div>                    
-                        <div
-                            className="image"
-                            onClick={() => {
-                                // Get the index of the image on click
-                                dispatch( setModalContent( currentPictures.indexOf(img) ) );
-                                // Then show the modal
-                                dispatch( showModal(true) );
-                            }}
-                        >
-                            <img src={img} alt={card.name}/>
-                        </div>
+        // Build card JSX
+        return (
+            <div className="column" key={card.arenaId}>
+                <div className="ui fluid card removeBoxShadow">
+                    <div className="content">
+                        <div className="right floated content" >{numOwned} / 4 </div>
+                    </div>                    
+                    <div
+                        className="image"
+                        // Display larger card image on click
+                        onClick={ () => {
+                            // Get the index of the image on click
+                            dispatch( setModalContent( currentPictures.indexOf(img) ) );
+                            // Then show the modal
+                            dispatch( showModal(true) );
+                        } }
+                    >
+                        <img src={img} alt={card.name}/>
                     </div>
                 </div>
-            )    
-        }
-        return null;
+            </div>
+        );
+        
     }); // end of the renderedCards map
 
-    // Update list of current images only once renderedCards has finished
+    // Track card images displayed, but only update redux state after JSX done rendering
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect( () => dispatch(updateImageList(currentPictures)), [renderedCards] );
 
     // Find optimal number of columns for card display based on screen width
     const width = useResizeWidth();
 
-    // initialize number of columns to five for full width screen
+    // Calculate how many columns of cards to show
     let numCols = 'five';
-    if ( width < 640 ) 
-        numCols = 'two';
-    else if ( width < 990 )
-        numCols = 'three';
-    else if ( width < 1200 )
-        numCols = 'four';
+    if      ( width < 640 )  numCols = 'two';
+    else if ( width < 990 )  numCols = 'three';
+    else if ( width < 1200 ) numCols = 'four';
     
-    return (
-        <>
-            <p className="ui container">Displaying <span className="numCardsShown">{numCardsShown}</span> card(s):</p>
-            <div className={`ui ${numCols} column grid container`}> 
-                {renderedCards}
-            </div>
-        </>
-    );
+    return (<>
+        {/* Counter for number of cards being displayed */}
+        <p className="ui container">
+            Displaying <span className="numCardsShown">{currentPictures.length}</span> card(s):
+        </p>
+
+        {/* JSX for matching cards */}
+        <div className={`ui ${numCols} column grid container`}> 
+            {renderedCards}
+        </div>
+    </>);
 }
-
-
 
 export default CardList;
