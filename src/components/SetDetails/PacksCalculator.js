f92789ed42d98cad224e9c038a22dd14288d8e3b
Alex-Van-Buren <76178479+Alex-Van-Buren@users.noreply.github.com>
2021-04-21 13:13:40 -0500

    Create and style PacksCalculator

--- src/components/SetDetails/PacksCalculator.js
+++ src/components/SetDetails/PacksCalculator.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import packsNeeded from '../../data/packsNeeded';
+import '../../css/PacksCalculator.css';
 
 function PacksCalculator() {
+    const { setId } = useParams();
+
+    // Grab values from redux and compute the average number of packs required to 100% complete rare collection
+    const raresOwned = useSelector( state => state.inventory.set[setId].rare.ownedTotal);
+    const raresTotal = useSelector( state => state.inventory.set[setId].rare.setTotal);
+    const packsToCompleteRares = packsNeeded(setId, 'rare' , raresOwned, raresTotal);
+
+    // Grab values from redux and compute the average number of packs required to 100% complete mythic collection
+    const mythicsOwned = useSelector( state => state.inventory.set[setId].mythic.ownedTotal);
+    const mythicsTotal = useSelector( state => state.inventory.set[setId].mythic.setTotal);
+    const packsToCompleteMythics = packsNeeded(setId, 'mythic' , mythicsOwned, mythicsTotal);
+
     return (
-        <div>Packs Calculator</div>
+        <div className="calculator"> 
+            <h2 >Average Number of Packs to Complete Collection</h2>       
+            <h3> Rare: <span className="packNumber">{packsToCompleteRares}</span> </h3>                        
+            <h3> Mythic: <span className="packNumber">{packsToCompleteMythics}</span> </h3>            
+        </div>
+
     );
 }
 
