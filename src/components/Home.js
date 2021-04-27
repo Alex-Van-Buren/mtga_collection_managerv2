0f9b88a9ead08240296aa317b58a6a12f6617880
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-04-27 13:26:29 -0500

    Toggle works backwards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import SetCard from './SetCard';
-import { standardSets as sets } from '../data/setInfo';
+import { standardSets, historicSets } from '../data/setInfo';
 import useResizeWidth from '../hooks/useResizeWidth';
-
-/**
- * Outputs JSX for each of the card sets to be displayed
- * - Current default sets are: 'eld', 'thb', 'iko', 'm21', 'znr', & 'khm'
- */
-function renderSets() {
-    
-    // Create each card set Component based on 'sets' input
-    const setCards = sets.map( (setId) => {
-        const path = `/set/${setId}`;
-
-        return (
-            <div className="ui link card" key={setId}>
-                <Link to={path}>
-                    <SetCard setId={setId} key={setId}/>
-                </Link>
-            </div>
-        );
-    });
-    
-    return (setCards);
-}
 
 /**
  * Home Page Component - Displays current card sets and other general info/options
@@ -44,10 +22,92 @@ function Home() {
     else if (width < 1200)
         numCards = "three";
 
+
+    // State determines whether to show standard or historic sets
+    const [showStandard, setShowStandard] = useState(true);
+
+    // Use local storage to decide whether to show standard or historic sets
+    useEffect(() => {
+        // Get value from local storage
+        const standardPreference = window.localStorage.getItem("showStandard");
+
+        if (standardPreference === "false") {
+            setShowStandard(false);
+        }
+        else if (standardPreference === "true") {
+            setShowStandard(true);
+        }
+    }, [])
+
+    // Use shown sets to determine title
+    const collectionTitle = showStandard ? "Standard Collection" : "Historic Collection";
+
+    const renderToggle = (
+        <div
+            // Accessibility
+            className="standardToggle" tabIndex="0"
+            onKeyDown={ e => {if (e.key === "Enter") toggleStandard(showStandard)} }
+            role="checkbox" aria-checked={showStandard} aria-label="Standard Mode Toggle"
+        >
+            <div id="standardLabel">Standard</div>
+
+            {/* Standard/Historic Toggle */}
+            <div className="ui fitted toggle checkbox">
+                <input 
+                    type="checkbox" name="StandardToggle" id="StandardToggle" tabIndex="-1"
+                    checked={showStandard} onChange={() => toggleStandard(showStandard)}
+                />
+                <label tabIndex="-1"></label>
+            </div>
+
+            <div id="historicLabel">Historic</div>
+        </div>
+    );
+
+    /**
+     * Toggles between standard and historic modes
+     * @param {boolean} standardEnabled True when standard is enabled
+     */
+    function toggleStandard(standardEnabled) {
+        if (standardEnabled) {
+
+            setShowStandard(false);
+            window.localStorage.setItem("showStandard", "false");
+        } else {
+
+            setShowStandard(true);
+            window.localStorage.setItem("showStandard", "true");
+        }
+    }
+
+    /**
+     * Outputs JSX for each of the card sets to be displayed
+     */
+    function renderSets() {
+
+        const sets = showStandard ? standardSets : historicSets;
+        
+        // Create each card set Component based on 'sets' input
+        const setCards = sets.map( (setId) => {
+            const path = `/set/${setId}`;
+
+            return (
+                <div className="ui link card" key={setId}>
+                    <Link to={path}>
+                        <SetCard setId={setId} key={setId}/>
+                    </Link>
+                </div>
+            );
+        });
+        
+        return (setCards);
+    }
+
     return(
         <div className="ui container">
-            <div className="ui huge center aligned header">Standard Collection</div>
+            <div className="ui huge center aligned header">{collectionTitle}</div>
             <div className={`ui ${numCards} cards`}>
+                {renderToggle}
                 {renderSets()}
             </div>
         </div>
