968f188255663f40fa8b943374f5425ac54f1994
Alex-Van-Buren <76178479+Alex-Van-Buren@users.noreply.github.com>
2021-05-03 11:46:58 -0500

    Move items div based on screen width

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 
+import useResizeWidth from '../hooks/useResizeWidth';
 import '../css/DropDown.css'
 
 function DropDown(props) {
@@ -9,18 +10,31 @@ function DropDown(props) {
     if (open) {
         titleClass +=" active";
     }
+    const items = (
+    <div className={open ? "dd-items" : "dd-items hidden"}>
+        <div className="dd-item ">Eldraine</div>
+        <div className="dd-item">Strixhaven</div>
+        <div className="dd-item">Ikoria</div>
+
+    </div>)
+
+    let hamburgerItems, normalItems ;
+    const width = useResizeWidth();
+    if ( width <= 739 ) {
+        hamburgerItems = items;
+        normalItems = null;
+    } else {
+        hamburgerItems = null;
+        normalItems = items;
+    }
 
     return (
         <>
         
-        <div className={titleClass} onClick={() => setOpen(!open)}>{props.title} <i className={open ? "icon chevron up" : "icon chevron down"}></i>            
-        </div>
-        <div className={open ? "dd-items" : "dd-items hidden"}>
-            <div className="dd-item ">Eldraine</div>
-            <div className="dd-item">Strixhaven</div>
-            <div className="dd-item">Ikoria</div>
-        
-        </div>
+            <div className={titleClass} onClick={() => setOpen(!open)}>{props.title} <i className={open ? "icon chevron up" : "icon chevron down"}></i>
+                {normalItems}
+            </div>
+            {hamburgerItems}
         </>
     )
 }
