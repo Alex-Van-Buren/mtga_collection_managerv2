b858622ff2abaad0443379d192dfe16b30f37c79
Alex-Van-Buren <76178479+Alex-Van-Buren@users.noreply.github.com>
2021-05-12 13:38:40 -0500

    More Logos

--- src/data/setInfo.js
+++ src/data/setInfo.js
@@ -1,6 +1,13 @@
 import stx_logo from '../images/setLogos/stx_logo.png';
 import khm_logo from '../images/setLogos/khm_logo.png';
 import znr_logo from '../images/setLogos/znr_logo.png';
+import akr_logo from '../images/setLogos/akr_logo.png';
+import m21_logo from '../images/setLogos/m21_logo.png';
+import iko_logo from '../images/setLogos/iko_logo.png';
+import thb_logo from '../images/setLogos/thb_logo.png';
+import eld_logo from '../images/setLogos/eld_logo.png';
+import m20_logo from '../images/setLogos/m20_logo.png';
+import war_logo from '../images/setLogos/war_logo.png';
 
 /**
  * The set codes of the current standard sets
@@ -41,37 +48,44 @@ export const setInfo = {
     akr: {
         name: "Amonkhet Remastered",
         mythic_upgrade: (1/6),
-        collationId: 100019
+        collationId: 100019,
+        logo: akr_logo
     },
     m21: {
         name: "Core Set 2021",
         mythic_upgrade: (1/8),
-        collationId: 100018
+        collationId: 100018,
+        logo: m21_logo
     },
     iko: {
         name: "Ikoria: Lair of Behemoths",
         mythic_upgrade: (1/8),
-        collationId: 100017
+        collationId: 100017,
+        logo: iko_logo
     },
     thb: {
         name: "Theros Beyond Death",
         mythic_upgrade: (1/8),
-        collationId: 100016
+        collationId: 100016,
+        logo: thb_logo
     },
     eld: {
         name:"Throne of Eldraine",
         mythic_upgrade: (1/8),
-        collationId: 100015
+        collationId: 100015,
+        logo: eld_logo
     },
     m20: {
         name: "Core Set 2020",
         mythic_upgrade: (1/8),
-        collationId: 100014
+        collationId: 100014,
+        logo: m20_logo
     },
     war: {
         name: "War of the Spark",
         mythic_upgrade: (1/8),
-        collationId: 100013
+        collationId: 100013,
+        logo: war_logo
     },
     rna: {
         name: "Ravnica Allegiance",
