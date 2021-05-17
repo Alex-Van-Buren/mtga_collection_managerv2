e808298129a0c93e743153fd557a886ba3964ca8
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-05-17 13:10:12 -0500

    Only iterate over keyEvents when defined

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { showModal } from '../../actions';
+import { showModal } from '../actions';
 
 /**
  * A generic modal that displays whatever content is passed to it as a prop. Closed when clicking off the modal or
@@ -11,7 +11,8 @@ import { showModal } from '../../actions';
  * @param {JSX} props.content The modal content to display.
  * @param {Array} props.keyEvents Describes the keys to listen for the the functions to execute upon that key being pressed.
  * - keyEvents is an Array of objects; each object contains a key and a function: { key: {Number}, keyFunction: {Function}}
- * - Events for key code 27 (escape) are ignored; it will always close the modal. 
+ * - Events for key code 27 (escape) are ignored; it will always close the modal.
+ * - Prevents default actions when key pressed.
  * @param {Number} props.keyEvents[].key The key to set an event listener for. Inside an object on the keyEvents array.
  * @param {Function} props.keyEvents[].keyFunction The function to execute when the key is pressed. Inside an object on
  * the keyEvents array.
@@ -38,14 +39,22 @@ function Modal({ content, keyEvents }) {
                     dispatch(showModal(false));
                 }
 
-                // Loop through each Object describing the pressed key and the function to execute
-                for (const { key, keyFunction } of keyEvents) {
+                // Only listen for additional keys if keyEvents is defined
+                if (keyEvents) {
 
-                    // Execute the function for the specified key code (unless it's escape)
-                    if (event.keyCode !== 27 && event.keyCode === key) {
-                        keyFunction();
+                    // Loop through each Object describing the pressed key and the function to execute
+                    for (const { key, keyFunction } of keyEvents) {
+    
+                        // Execute the function for the specified key code (unless it's escape)
+                        if (event.keyCode !== 27 && event.keyCode === key) {
+    
+                            event.preventDefault();
+    
+                            keyFunction();
+                        }
                     }
                 }
+
             }
             
             // Add listener for function
