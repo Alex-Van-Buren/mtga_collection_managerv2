d48602b97764fba3b467b76eb5eb28d1317ab456
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-05-19 11:14:40 -0500

    Added 404 Pages

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Footer from './Footer';
 import Home from './Home';
 import SetDetails from './SetDetails/SetDetails';
 import Help from './Help';
+import PageNotFound from './PageNotFound';
 import ScrollTop from './ScrollTop';
 import '../css/App.css';
 
@@ -22,7 +23,8 @@ function App() {
                     <Switch>
                         <Route exact component={Home} path='/' />
                         <Route component={SetDetails} path='/set/:setId' />
-                        <Route component={Help}       path='/help'/>
+                        <Route component={Help}       path='/help' />
+                        <Route component={PageNotFound} />
                     </Switch>
 
                     <ScrollTop />
