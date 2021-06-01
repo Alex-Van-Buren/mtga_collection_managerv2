4d6c072be365d53779af2c3065470d6a6220d29e
Alex-Van-Buren <76178479+Alex-Van-Buren@users.noreply.github.com>
2021-06-01 13:26:13 -0500

    Add FAQs and contact

--- src/components/Help.js
+++ src/components/Help.js
@@ -12,9 +12,11 @@ import detailedLogsImg from '../images/arena/detailedLogs.png';
 function Help() {
 
     return (
+    <>
+    <h1 className='helpTitle'>Help</h1>
     <div className=' ui container helpPage'>
        
-        <div>
+        <div className='findLogFile'>
             <h2>Finding Your Log file</h2>
             <p>Your log file is an automatically generated file created by MTG Arena named "Player.log". It can be 
                 found at one of the following locations inside the drive where Arena is installed:
@@ -41,10 +43,31 @@ function Help() {
 
         <div className="FAQ">
             <h2>FAQ's</h2>
+            <ol>
+                <li>What does "Collection Manager" do with my log file/info?
+                    <ul>
+                        <li>When a user uploads their log file, they are allowing their own browser to analzye it. We do NOT keep
+                        your log file or information.</li>
+                    </ul>
+                </li>
+                <li>How do I upload my log file on mobile?
+                    <ul>
+                        <li>While "Collection Manager" can be used on mobile, the mobile application for MTG Arena does not seem to 
+                        generate a log file that "Collection Manager" can parse.</li>
+                    </ul>
+                </li>
+            </ol>
 
         </div>
 
-    </ div>);
+        <div className="contact">
+            <h2>Contact Us</h2>
+            <p>If you have any questions/concerns feel free to message us at: <span className='contactInfo'>ADD ACTUAL CONTACT INFO</span></p>
+        </div>
+
+    </ div>
+    </>
+    );
 }
 
 export default Help;
