19558998f47aae79fe3dcb95746633ed97cc14b0
Alex-Van-Buren <76178479+Alex-Van-Buren@users.noreply.github.com>
2021-06-02 12:20:30 -0500

    Accordion opens and closes with animation

--- src/components/Templates/Accordion.js
+++ src/components/Templates/Accordion.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+
+import '../../css/Accordion.css';
 
 function Accordion(props) {
     const [open, setOpen] = useState(false);
+    const panelRef = useRef();
+
+    useEffect(() => {
+        if (open) {
+            panelRef.current.style.maxHeight = panelRef.current.scrollHeight +"px";
+        } else {
+            panelRef.current.style.maxHeight = 0;
+        }
+    },[open])
 
     // Copy All the children elements and put into an array
     const elements = React.Children.toArray(props.children);
@@ -16,16 +27,21 @@ function Accordion(props) {
     )
 
     // Put the expand button as a clild of the first element
-    let firstElementChild = [elements[0].props.children, expandButton];
-    let firstElement = React.cloneElement(elements[0], {}, firstElementChild );
+    const firstElementChildren = [elements[0].props.children, expandButton];
+    const firstElement = React.cloneElement(elements[0], {}, firstElementChildren );
 
     // Put the rest of the elements into a div that will show/hide based on open state
-    
-    let renderElements = [firstElement, ...elements.slice(1)];
+    const collapsableStuff = (
+        <div className={open ? "panel panelExpanded" : "panel panelCollapsed"} ref={panelRef}>
+            {elements.slice(1)}
+        </div>
+    )
+
 
     return(
         <>
-            {renderElements} 
+            {firstElement}
+            {collapsableStuff} 
         </>
     )
 }
