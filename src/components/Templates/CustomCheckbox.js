5f692f53eb3a3fc129c7be1c1860846759ca2897
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-06-10 17:58:32 -0500

    Recommit Alex's CMC branch changes

--- src/components/Templates/CustomCheckbox.js
+++ src/components/Templates/CustomCheckbox.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 /**
  * A custom checkbox
- * @param {string} text Text for the checkbox
- * @param {boolean} checked True/false the checkbox is checked
- * @param {function} onChange Function to execute on checkbox change
- * @param {string} labelClass Class name(s) for the label
- * @param {string} inputClass Class name(s) for the input
+ * @param {string} props.text Text for the checkbox
+ * @param {boolean} props.checked True/false the checkbox is checked
+ * @param {function} props.onChange Function to execute on checkbox change
+ * @param {string} props.labelClass Class name(s) for the label
+ * @param {string} props.inputClass Class name(s) for the input
  * @returns Checkbox JSX
  */
 export default function CustomCheckbox({ text, labelText=text, checked, onChange, labelClass, inputClass }) {
