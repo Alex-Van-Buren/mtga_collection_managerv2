98ba0637ff97f462ea8d092ede00a36d37009740
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-06-24 10:12:57 -0500

    Scroll throttling

--- src/components/Templates/LazyLoad.js
+++ src/components/Templates/LazyLoad.js
@@ -16,27 +16,59 @@ function LazyLoad(
     const childrenPerRow = useMemo(() => Math.floor(viewWidth/childWidth), [viewWidth, childWidth]);
     const height = useMemo(() => (children.length/childrenPerRow * childHeight), [children, childrenPerRow, childHeight]);
 
-    // Add listener for Y scrolling
+    // Add listener for Y scrolling on page load or resize
     useEffect(() => {
+        
+        // Watch for scrolling and compute number of children to show
         function onScrollY() {
 
-            // Calculate number of children to show
-            const numChildrenWeNeedToShow = Math.ceil((window.scrollY + viewHeight) / childHeight) * childrenPerRow;
+            throttle(() => { // Function to throttle
 
-            // Increment number of children to show if user scrolls down
-            if (numChildrenWeNeedToShow > numChildrenShown) {
-                setNumChildrenShown(numChildrenWeNeedToShow);
+                // Calculate number of children to show
+                const numChildrenWeNeedToShow = Math.ceil((window.scrollY + viewHeight) / childHeight) * childrenPerRow;
+    
+                // Increment number of children to show if user scrolls down
+                if (numChildrenWeNeedToShow > numChildrenShown) {
+                    setNumChildrenShown(numChildrenWeNeedToShow);
+                }
+
+            }, 200); // Delay
+
+        }
+
+        // Create a timer id
+        let id;
+
+        // Throttle scrolling event function
+        function throttle(throttledFunction, delay) {
+
+            // Do nothing if the timeout hasn't completed yet (otherwise id would be undefined)
+            if (id) {
+                return;
             }
+
+            // Call {throttledFunction} after {delay} and set timeout id
+            id = setTimeout(() => {
+
+                // Run the throttled function after the delay
+                throttledFunction();
+
+                // And clear the timer id
+                id = undefined;
+            }, delay);
         }
 
         //TODO: clean up event listener when done, call less often?, load more than a row at a time?, track scrollY?
 
         // Track scrolling
         window.addEventListener("scroll", onScrollY);
-
+        
         // Cleanup
         return () => window.removeEventListener("scroll", onScrollY);
-    }, [childHeight, viewHeight, childrenPerRow, numChildrenShown]);
+
+    }, [childHeight, childrenPerRow, numChildrenShown, viewHeight]);
+    
+    
 
     // Set height of lazyload div so scroll bar shows true length
     return <div className="lazyLoad" style={{ "height" : {height} }} >
