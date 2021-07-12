5b86d100a8c4879f83dee167638fdf5c66223a13
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-07-12 11:38:42 -0500

    Intermediate changes

--- src/components/DeckBuilder/DBDeck.js
+++ src/components/DeckBuilder/DBDeck.js
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { removeCardFromDeck } from '../../actions';
 import '../../css/DBDeck.css';
 
 function DBDeck() {
 
+    // 8 cmcs for cards. Default sort is cmc.
+    const [col0, setCol0] = useState([]);
+    const [col1, setCol1] = useState([]);
+    const [col2, setCol2] = useState([]);
+    const [col3, setCol3] = useState([]);
+    const [col4, setCol4] = useState([]);
+    const [col5, setCol5] = useState([]);
+    const [col6, setCol6] = useState([]);
+    const [col7, setCol7] = useState([]);
+
+    const col = [ col0, col1, col2, col3, col4, col5, col6, col7 ];
+    const setCol = [ setCol0, setCol1, setCol2, setCol3, setCol4, setCol5, setCol6, setCol7 ];
+
     const dispatch = useDispatch();
 
-    const deck = useSelector(state => state.deckBuilder.deckList);
+    // See when cards need to be added to the deck
+    const cardToAdd = useSelector(state => state.deckBuilder.add);
 
-    const deckMap = deck.map((card) => {
-        return (
-            <img 
-                src={card.imgs.front} alt={card.name} style={{width: '200px'}}
-                onClick={() => dispatch(removeCardFromDeck(card))}
-            />
-        );
-    });
+    useEffect(() => {
+
+        // Find where to 
+    }, [cardToAdd]);
+
+    /**
+     * Converts cmc into column number 0-7
+     */
+    function colNumber(cmc) {
+        if (typeof cmc === 'string') {
+            cmc = parseInt(cmc);
+        }
+        // Minimum value of cmc is 0 and map undefined/null to 0
+        if (!cmc || cmc < 0) {
+            cmc = 0;
+        }
+        // Max cmc value is 7
+        else if (cmc > 7) {
+            cmc = 7;
+        }
+        return cmc;
+    }
 
     return (
         <div id="DBDeck">
-            {deckMap}
+            {}
         </div>
     );
 }
 
 export default DBDeck;
-
-// Doubly linked list node
-class node {
-    constructor(card, next=null, prev=null) {
-        this.next = next;
-        this.prev = prev;
-        this.card = card;
-    }
-}
