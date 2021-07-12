3c3bc159f6fc0993f7c24a0a019cada36190fe3f
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-07-12 13:48:20 -0500

    DBDeck now uses redux

--- src/components/DeckBuilder/DBDeck.js
+++ src/components/DeckBuilder/DBDeck.js
@@ -1,66 +1,15 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 
 import '../../css/DBDeck.css';
 
 function DBDeck() {
 
-    // Store deck data in 8 columns
-    const [col0, setCol0] = useState([]);
-    const [col1, setCol1] = useState([]);
-    const [col2, setCol2] = useState([]);
-    const [col3, setCol3] = useState([]);
-    const [col4, setCol4] = useState([]);
-    const [col5, setCol5] = useState([]);
-    const [col6, setCol6] = useState([]);
-    const [col7, setCol7] = useState([]);
-
-    // Make state easily accessible
-    const col = [ col0, col1, col2, col3, col4, col5, col6, col7 ];
-    const setCol = [ setCol0, setCol1, setCol2, setCol3, setCol4, setCol5, setCol6, setCol7 ];
-
-    // See when cards need to be added to the deck
-    const cardToAdd = useSelector(state => state.deckBuilder.add);
-
-    // Add cards to the correct column
-    useEffect(() => {
-
-        if (cardToAdd) {
-
-            // Find which column to add card to
-            const i = colNumber(cardToAdd.cmc);
-
-            // Copy current state
-            const state = [ ...col[i], cardToAdd ];
-
-            // Push new state
-            setCol[i](state);
-        }
-
-    // Don't want to add col and setCol to useEffect dependencies
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cardToAdd]);
-
-    /**
-     * Converts cmc into column number 0-7
-     */
-    function colNumber(cmc) {
-        if (typeof cmc === 'string') {
-            cmc = parseInt(cmc);
-        }
-        // Minimum value of cmc is 0 and map undefined/null to 0
-        if (!cmc || cmc < 0) {
-            cmc = 0;
-        }
-        // Max cmc value is 7
-        else if (cmc > 7) {
-            cmc = 7;
-        }
-        return cmc;
-    }
+    // Get deck from redux
+    const deck = useSelector(state => state.deckBuilder.deck);
 
     // Make an array of JSX for each of the 8 deck columns
-    const renderCards = col.map(column => {
+    const renderCards = deck.map(column => {
         return (
             <div className="DBDeckColumn">
 
--- src/reducers/deckBuilderReducer.js
+++ src/reducers/deckBuilderReducer.js
@@ -1,7 +1,7 @@
 import { ADD_CARD_TO_DECK } from '../actions/types';
 
 const INITIAL_STATE = {
-    add: null
+    deck: [ [], [], [], [], [], [], [], [] ]
 };
 
 export default function deckbuilderReducer(state = INITIAL_STATE, action) {
@@ -9,9 +9,40 @@ export default function deckbuilderReducer(state = INITIAL_STATE, action) {
 
         // Add card to be added or removed to redux
         case ADD_CARD_TO_DECK: {
-            return { ...state, add: action.payload };
+
+            const cardToAdd = action.payload;
+
+            // Find which column to add card to
+            const i = colNumber(cardToAdd.cmc);
+
+            // Copy current state
+            const newDeck = [ ...state.deck ];
+
+            // Add card to state
+            newDeck[i].push(cardToAdd);
+
+            // Update state
+            return { ...state, deck: newDeck };
         }
         default:
             return state;
     }
+}
+
+/**
+     * Converts cmc into column number 0-7
+     */
+ function colNumber(cmc) {
+    if (typeof cmc === 'string') {
+        cmc = parseInt(cmc);
+    }
+    // Minimum value of cmc is 0 and map undefined/null to 0
+    if (!cmc || cmc < 0) {
+        cmc = 0;
+    }
+    // Max cmc value is 7
+    else if (cmc > 7) {
+        cmc = 7;
+    }
+    return cmc;
 }
