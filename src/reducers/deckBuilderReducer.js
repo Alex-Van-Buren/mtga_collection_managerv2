0aa4b683120efd1917bf9204ffc6966e29db1f06
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-07-14 19:48:05 -0500

    Add deckMap to redux

--- src/reducers/deckBuilderReducer.js
+++ src/reducers/deckBuilderReducer.js
@@ -1,7 +1,10 @@
 import { ADD_CARD_TO_DECK, REMOVE_CARD_FROM_DECK, SELECT_DECK_TYPE } from '../actions/types';
 
 const INITIAL_STATE = {
+    // Array of card columns, each corresponds to cmc value 0-7, >7 mapped to 7
     deck: [ [], [], [], [], [], [], [], [] ],
+    // Contains card names, ids, and number of copies
+    deckMap: {}, // key: card.name, value: { key: card.arendId, value: number in deck }
     deckType: "standard"
 };
 
@@ -11,35 +14,72 @@ export default function deckbuilderReducer(state = INITIAL_STATE, action) {
         // Add card to be added or removed to redux
         case ADD_CARD_TO_DECK: {
 
-            const cardToAdd = action.payload;
+            const card = action.payload;
 
             // Find which column to add card to
-            const i = colNumber(cardToAdd.cmc);
+            const i = colNumber(card.cmc);
 
             // Copy current state
             const newDeck = [ ...state.deck ];
+            const newDeckMap = { ...state.deckMap };
 
-            // Add card to state
-            newDeck[i].push(cardToAdd);
+            // Add card to deck
+            newDeck[i].push(card);
+
+            /* Add card to deckMap */
+
+                // Initialize specific card name in deckMap if necessary
+                if (!newDeckMap[card.name]) {
+                    newDeckMap[card.name] = {};
+                }
+
+                // Increment count if specific arenaId declared under card name
+                if (newDeckMap[card.name][card.arenaId]) {
+                    newDeckMap[card.name][card.arenaId]++;
+                }
+                // Else initialize arenaId for card name
+                else {
+                    newDeckMap[card.name][card.arenaId] = 1;
+                }
+
+            /* End adding to deckMap */
 
             // Update state
-            return { ...state, deck: newDeck };
+            return { ...state, deck: newDeck, deckMap: newDeckMap };
         }
         case REMOVE_CARD_FROM_DECK: {
 
-            const cardToRemove = action.payload;
+            const card = action.payload;
 
             // Find which column to add card to
-            const i = colNumber(cardToRemove.cmc);
+            const i = colNumber(card.cmc);
 
             // Copy current state
             const newDeck = [ ...state.deck ];
+            const newDeckMap = { ...state.deckMap };
 
-            // Remove card from state
-            newDeck[i].splice(newDeck[i].indexOf(cardToRemove), 1);
+            // Remove card from deck
+            newDeck[i].splice(newDeck[i].indexOf(card), 1);
+
+            /* Remove card from deckMap */
+
+                // Decrement card count
+                newDeckMap[card.name][card.arenaId]--;
+
+                // Check if arenaId needs to be removed
+                if (newDeckMap[card.name][card.arenaId] <= 0) {
+                    delete newDeckMap[card.name][card.arenaId];
+                }
+
+                // Check if card name needs to be removed
+                if (Object.keys(newDeckMap[card.name]).length <= 0) {
+                    delete newDeckMap[card.name];
+                }
+
+            /* End removing from deckMap */
 
             // Update state
-            return { ...state, deck: newDeck };
+            return { ...state, deck: newDeck, deckMap: newDeckMap };
         }
         case SELECT_DECK_TYPE: {
             
