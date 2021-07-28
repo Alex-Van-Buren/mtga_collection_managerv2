a8df57aada8e55746b9d96f13f048861cfefc242
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-07-28 12:40:56 -0500

    Styling

--- src/components/DeckBuilder/DBDeckOptions.js
+++ src/components/DeckBuilder/DBDeckOptions.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { selectDeckType } from '../../actions';
 import CustomDropdown from '../Templates/CustomDropdown';
 import ImportExportModal from './ImportExportModal';
-import { setDeck, setSideboard, changeCommander, changeCompanion } from '../../actions'
+import { selectDeckType, setDeck, setSideboard, changeCommander, changeCompanion } from '../../actions'
 import '../../css/DBDeckOptions.css';
 
 function DBDeckOptions() {
