ce9720626a482a1916ad09851d94e786eb8cc36d
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-08-31 12:21:21 -0500

    Clear Deck -> Reset to Sideboard in limited mode

--- src/components/DeckBuilder/DBDeckOptions.js
+++ src/components/DeckBuilder/DBDeckOptions.js
@@ -3,57 +3,64 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import CustomDropdown from '../Templates/CustomDropdown';
 import ImportExportModal from './ImportExportModal';
-import { selectDeckType, setDeck, setSideboard, changeCommander, changeCompanion } from '../../actions'
+import { selectDeckType, setDeck, setSideboard, changeCommander, changeCompanion } from '../../actions';
 import '../../css/DBDeckOptions.css';
 
+/**
+ * Options that affect the deck in the deck builder. Includes the deck type (e.g. standard, limited, etc.), import and
+ * export buttons, and clear/reset deck button.
+ */
 function DBDeckOptions() {
     
     const dispatch = useDispatch();
-    const selectedDeckType = useSelector(state => state.deckBuilder.deckType);
-    const currentDeck      = useSelector(state => state.deckBuilder.deck);
-    const currentSideboard = useSelector(state => state.deckBuilder.sideboard);
+    const { deckType, deck, sideboard } = useSelector(state => state.deckBuilder);
     
     // Deck/game type
     const items = [ "standard", "historic", "limited", "brawl", "custom" ];
     const renderGameType = <CustomDropdown
-        items={items} firstSelection={selectedDeckType}
+        items={items} firstSelection={deckType}
         selectfn={(item) => dispatch(selectDeckType(item))}
         ariaLabel="Select deck type"
     />
 
     // Reset deck
     function clearDeck() {
+
         // Check the deckType
-        if (selectedDeckType === 'limited' ){
+        if (deckType === 'limited' ){
+
             // If creating limited deck --> move all cards from deck to sideboard, then clear deck, companion and commander
-            const newSideboard = currentDeck.concat(currentSideboard).flat();
             dispatch(setDeck([]));
-            dispatch(setSideboard(newSideboard));
+            dispatch(setSideboard(deck.concat(sideboard).flat()));
             dispatch(changeCommander(null));
             dispatch(changeCompanion(null));
 
         } else {
-            // Otherwise clear main deck, sideboard, companion, and commander
+
+            // Clear deck, sideboard, companion, and commander
             dispatch(setDeck([]));
             dispatch(setSideboard([]));
             dispatch(changeCommander(null));
             dispatch(changeCompanion(null));
         }
     }
+
     const clearButton = (
         <button className="clearDeck"
-        onClick={() => clearDeck()}
+            onClick={() => clearDeck()}
         >
-            Clear Deck
+            {deckType === "limited" ? "Reset to Sideboard" : "Clear Deck"}
         </button>
-    )
+    );
 
     return (
         <div id="DBDeckOptions">
+
             <label htmlFor="gameTypeDropDown">Deck Type:</label>
             <div id="gameTypeDropDown">
                 {renderGameType}
             </div>
+
             <ImportExportModal />
             {clearButton}
         </div>
