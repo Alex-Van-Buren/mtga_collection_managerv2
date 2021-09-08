35ff5b7ac852184b40fb0dacad7aa101318b3e65
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-09-08 12:55:11 -0500

    Update announcements

--- src/components/All/Announcements.js
+++ src/components/All/Announcements.js
@@ -11,7 +11,6 @@ function Announcements() {
     const [modalOpen, setModalOpen] = useState(true);
 
     // Check if announcement has been made this session
-    // eslint-disable-next-line eqeqeq
     if (window.sessionStorage.getItem("seenAnnouncements") === "true") {
 
         // Don't show this modal when announcements have been seen this session
@@ -42,26 +41,13 @@ function Announcements() {
             <ol>
                 <li>
                     <strong>This site is in pre-release!</strong>
-                    <p>The site URL will soon be moving to a permanent domain (i.e. <strong>NOT</strong> https://alex-van-buren.github.io/)</p>
-                    <p>Performance optimizations: cards now load only once they're about to enter the screen. This should speed up the site.</p>
-                    <p>Further performance optimizations will come once the permanent site is established.</p>
                 </li>
                 <li>
-                    <strong>The Deck Builder is finally here... But it's not done yet!</strong>
-                    <p>The deck builder is in active development. Some features are missing, but most features work on the desktop version.</p>
-                    <p>Use the Filters on the left side to change which cards you see, advanced filters are available under the three-cogs icon.</p>
-                    <p>Click or drag & drop cards into your deck, sideboard, commander, or companion.</p>
-                    <p>Import or export a deck from/to file or clipboard.</p>
-                    <p>Some drag & drop features do not currently work in "Limited" mode.</p>
+                    <strong>Log Files are still broken</strong>
+                    <p>We will update Collection Manager as soon as MTG Arena updates their log files!</p>
                 </li>
                 <li>
-                    <strong>Log Files are currently broken!</strong>
-                    <p>The recent update removed your currently owned cards from the log files, so we can't show you which cards you currently own.</p>
-                    <p>Features related to your currently owned cards will be reimplemented as soon as a relevant patch is released.</p>
-                </li>
-                <li>
-                    <strong>Jumpstart: Historic Horizons is here!</strong>
-                    <p>Card images are currently "Placeholder Images". But will be updated once Scryfall updates their data.</p>
+                    <strong>Check out the deck builder</strong>
                 </li>
             </ol>
         </div>
