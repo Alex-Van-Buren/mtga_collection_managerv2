3894094ec845020204eb60ce8863e96a1f96e3fb
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-09-23 13:03:51 -0500

    Announcements - local storage + manual tracking

--- src/components/All/Announcements.js
+++ src/components/All/Announcements.js
@@ -13,7 +13,7 @@ function Announcements() {
     const version = 'v0.6.3';
 
     // Specify date of newest announcement. Announcements hidden after this date won't be shown.
-    const newestAnnouncement = 20210921000000; // YYYY + MM + DD + 000000
+    const newestAnnouncement = 20210922000000; // YYYY + MM + DD + 000000
 
     const [modalOpen, setModalOpen] = useState(true);
 
@@ -46,15 +46,19 @@ function Announcements() {
         <div id="announceList">
             <ol>
                 <li>
-                    <strong>Innistrad: Midnight Hunt is now available!</strong>
+                    <strong>Your card collection is now stored in your browser!</strong>
+                    <p>Clearing your cookies + site data for "all time" will also clear your collection! (Clearing less than "all time" is safe though)</p>
                 </li>
                 <li>
-                    <strong>MTG Arena's log file are still completely broken.</strong>
-                    <p>We are working on a manual mode for collection tracking. </p>
-                </li>
-                <li>
-                    <strong>New card legalities available</strong>
-                    <p>Historic Brawl and Future legalities are now available as Game Modes in the Deck Builder!</p>
+                    <strong>Manual Collection Tracking is now available!</strong>
+                    <p>Log files are still broken, so automatic tracking via uploading a log file is still unavailable.</p>
+                    <p>Directions:</p>
+                    <ul>
+                        <li>Click on the set you want to track</li>
+                        <li>Find the card that you want to update by either scrolling to it or using card filters</li>
+                        <li>Hover over the number owned (X/4)</li>
+                        <li>The buttons to change how many you own will appear!</li>
+                    </ul>
                 </li>
                 <li>
                     <strong>This site is in pre-release!</strong>
