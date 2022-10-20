95539a017c6b070c5f1ff545f8f1112707b2fdfd
Nate-Sackett <Nate.A.Sackett@gmail.com>
2022-10-20 12:02:08 -0500

    Updates

--- src/data/arenaCards.js
+++
@@ -1,3 +0,0 @@
-import arenaCards from './arenaCards20221019160904.json';
-
-export default arenaCards;
--- src/hooks/getTime.js
+++
@@ -1,4 +0,0 @@
-/** Returns the current UTC time as a string of only numbers. */
-export default function getTime() {
-    return `${new Date().toISOString().replace(/\..+/g, "").replace(/[T:-]/g, "")}`;
-}
